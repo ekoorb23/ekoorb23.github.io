f5a4851076062f453b6c091db71975801c515269
ekoorb23 <155214346+ekoorb23@users.noreply.github.com>
2023-12-31 13:05:55 +0930

    Upload module 01

---
+++ Fundamentals/01/story_content/6QHa8bDyCBg_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.55.25975.0%0D%0A%0D%0A00:00:00.040%20--%3E%2000:00:01.933%0D%0AAcacia%20keeps%20a%20digital%20record.%0D%0A%0D%0A"}')
---
+++ Fundamentals/01/story_content/6TuW1nAMft7_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.55.25975.0%0D%0A%0D%0A00:00:00.128%20--%3E%2000:00:02.064%0D%0AWelcome%20to%20the%20Acacia%201.0%0D%0A%0D%0A00:00:02.065%20--%3E%2000:00:04.652%0D%0APatient%20Administration+%20Fundamentals%20training.%0D%0A%0D%0A"}')
