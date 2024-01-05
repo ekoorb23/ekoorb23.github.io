4848a4aab98c935232e214c6869c986741ca0bd7
ekoorb23 <155214346+ekoorb23@users.noreply.github.com>
2024-01-05 18:49:34 +0930

    Partial upload of module 10

---
+++ PMI/10/story_content/5ch3llRIbyw_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.64.27845.0%0D%0A%0D%0A00:00:00.000%20--%3E%2000:00:03.624%0D%0AYou%20may%20need%20to%20scroll%20down%20to%20see%20the%20Privacy%20status%20field.%0D%0A%0D%0A"}')
---
+++ PMI/10/story_content/62uy6CBv1VV_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.65.28121.0%0D%0A%0D%0A00:00:00.000%20--%3E%2000:00:04.732%0D%0ACynthia%E2%80%99s%20name%20now%20appears%20as%20%E2%80%9CAnonymous%E2%80%9D%20and%20her%20name%20cannot%20be%20edited.%20%0D%0A%0D%0A"}')
